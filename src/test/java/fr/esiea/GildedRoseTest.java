d72800d6ba51ed82cf37c89cd35788ccfdcff31e
IvenKor <pauulviard@gmail.com>
2018-01-15 14:32:56 +0100

    Add tests about an Aged Brie

--- src/test/java/fr/esiea/GildedRoseTest.java
+++ src/test/java/fr/esiea/GildedRoseTest.java
@@ -15,4 +15,30 @@ public class GildedRoseTest {
         assertEquals(9 , app.items[0].quality);
     }
 
+    @Test
+    public void agedBrieAfterOneDay(){
+        Item[] items = new Item[] {new Item("Aged Brie", 10, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1 , app.items[0].quality);
+    }
+
+    @Test
+    public void agedBrieAfter50Days(){
+        Item[] items = new Item[] {new Item("Aged Brie", 50, 0)};
+        GildedRose app = new GildedRose(items);
+        for(int i=0; i<50; i++)
+            app.updateQuality();
+        assertEquals(50 , app.items[0].quality);
+    }
+
+    @Test
+    public void agedBrieAfter50DaysPassed(){
+        Item[] items = new Item[] {new Item("Aged Brie", 1, 0)};
+        GildedRose app = new GildedRose(items);
+        for(int i=0; i<50; i++)
+            app.updateQuality();
+        assertEquals(50 , app.items[0].quality);
+    }
+
 }
