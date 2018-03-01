9db289bca296e951857805cb754df32a2356051e
AlexisDanjon <alientgp@hotmail.fr>
2018-03-01 01:13:07 +0100

    Items in ArrayList - addItem - getItem

--- src/main/java/fr/esiea/GildedRose.java
+++ src/main/java/fr/esiea/GildedRose.java
@@ -1,17 +1,50 @@
 package fr.esiea;
 
-public class GildedRose {
-    Item[] items;
+import java.util.regex.*;
+import java.util.ArrayList;
 
-    public GildedRose(Item[] items) {
+public class GildedRose {
+    ArrayList<Item> items = new ArrayList<Item>();
+
+    public GildedRose(ArrayList<Item> items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            items[i].update();
+        for (Item item : items) item.update();
+    }
+    
+    public void addItem() {
+        Item item;
+        String name;
+        int sellIn, quality;
+        Scanner scanner = new Scanner( System.in );
+        System.out.print( "Type a name: " );
+        name = scanner.nextLine();
+        System.out.print( "Type a sell in number: " );
+        sellIn = scanner.nextInt();
+        System.out.print( "Type a quality number: " );
+        quality = scanner.nextInt();
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        Matcher m = p.matcher(name);
+        boolean nb = m.matches();
+        p = Pattern.compile("[0-9]+");
+        m = p.matcher(sellIn);
+        boolean sb = m.matches();
+        m = p.matcher(quality);
+        boolean qb = m.matches();
+        if(nb && sb && qb){
+            item = new Item(name, sellIn, quality);
+            items.add(item);
+        }
+        else{ 
+            System.out.println("Try again !");
         }
     }
-
+    
+     public Item returnItem() {
+        if(items != null) return items;
+        else System.out.println("Items not found");
+    }
 
 }
